87b7a039b529c17eb721dd5fb17845b0fd80c840
medinajavierj <javier_j_medina@hotmail.com>
2025-02-04 19:07:49 -0300

    rutatabla

--- routes/argentina.routes.js
+++ routes/argentina.routes.js
@@ -8,6 +8,9 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 const FIXTURE_PATH = path.join(__dirname, '../matches_arg/all/full_fixture.json');
+const TABLA_ANUAL_PATH = path.join(__dirname, '../matches_arg/tables/tablaAnual.json');
+const PROMEDIOS2025_PATH = path.join(__dirname, '../matches_arg/tables/promedios2025.json');
+const GRUPOS_PATH = path.join(__dirname, '../matches_arg/tables/grupos.json');
 
 router.get("/", (req, res) => {
   res.send("Welcome to api argentina");
@@ -40,5 +43,39 @@ router.get('/round/:id', (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+//ruta de tablas 
+router.get('/anual', (req, res) => {
+  try {
+    const data = readJsonFile(TABLA_ANUAL_PATH);
+    if (!data) return res.status(404).json({ error: 'No se encontró la tabla anual' });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.get('/promedios', (req, res) => {
+  try {
+    const data = readJsonFile(PROMEDIOS2025_PATH);
+    if (!data) return res.status(404).json({ error: 'No se encontró la tabla de promedios' });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Grupos
+router.get('/grupos', (req, res) => {
+  try {
+    const data = readJsonFile(GRUPOS_PATH);
+    if (!data) return res.status(404).json({ error: 'No se encontraron los grupos' });
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 export default router;
